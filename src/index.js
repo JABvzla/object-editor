446944f7f86fcac775f496b23a305f6fb0485a94
JABvzla <joseantoniobonito@hotmail.com>
2019-05-05 14:21:39 -0300

    add enviroments

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class ObjectEditor {
+  constructor(id, _cfg) {
+    ReactDOM.render(<App />, document.getElementById(id));
+  }
+}
+
+if (process.env.NODE_ENV === "development") new ObjectEditor("object-editor");
+
+window.ObjectEditor = ObjectEditor;
